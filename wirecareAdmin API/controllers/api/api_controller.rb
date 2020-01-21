db5769ed1285dd9b1f055e2ea2598388293d08c6
Jake <mccarthyjacob9@gmail.com>
2020-01-21 10:09:26 -0500

    updated comments for clarification

--- wirecareAdmin API/controllers/api/api_controller.rb
+++ wirecareAdmin API/controllers/api/api_controller.rb
@@ -1,11 +1,10 @@
-# INCLUDED FOR A MORE COMPLETE PICTURE
 class Api::ApiController < ApplicationController
-  skip_before_action :rvu #simplified for security reasons...
-  skip_before_filter :vat
+  skip_before_action :require_valid_user
+  skip_before_filter :verify_authenticity_token
   before_action :require_localhost
-  before_action :ensure_500 #if we don't have JSON format, should just error without wasting time. 
-  # THE FOLLOWING CODE IS RELEVENT 
-  rescue_from ApiExceptions::BaseException, with: :render_api_error_response 
+  # THE FOLLOWING CODE WAS ADDED TO DEAL WITH ERRORS IN THE API
+  before_action :ensure_500
+  rescue_from ApiExceptions::BaseException, with: :render_api_error_response
 
   def render_api_error_response(error)
     render json: {"status": "error", "message": error.message}.to_json
@@ -16,11 +15,10 @@ class Api::ApiController < ApplicationController
     return if request.format == :json
     render head: 500, status: 500
   end
-  
-  # END OF RELEVENT CODE 
+# END OF CODE ADDED BY JAKE
   def require_localhost
     return unless Rails.env.production?
-    ip_list = []# this array contained ip's, so they have been erased per prior employers request. 
+    ip_list = []# this array contained ip's, so they have been erased. 
     render_404 unless ip_list.include?(request.remote_ip.to_s)
   end
 
